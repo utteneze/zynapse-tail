50c2a7c4460e3b0fcfe52c306862d77d9f8b3ce2
Alan <alan_biju@outlook.com>
2024-06-01 00:10:04 +0530

    feat: connect mongoose

---
+++ src/shared/database/setup.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+import env from '../../config/environment.js';
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(env.DATABASE_URL);
+        console.log(
+            `🍃 [database] Established connection with MongoDB @ ${conn.connection.host}`
+        );
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.error(`🍃 [database]  ${error.message}`);
+        }
+        console.log('Are you sure MongoDB is running?');
+        process.exit(1);
+    }
+};
+
+export default connectDB;
